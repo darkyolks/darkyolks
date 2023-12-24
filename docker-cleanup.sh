a09286014ee24b4a14939a041331a515b40d847a
Grant Wilsey <99307736+darkyolks@users.noreply.github.com>
2023-12-24 02:09:26 -0800

    Create docker-cleanup.sh

---
+++ docker-cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Practical API Hacking course//TCM Security
+
+sudo docker stop $(sudo docker ps -aq)
+sudo docker rm $(sudo docker ps -aq)
+sudo docker rmi $(sudo docker images -q)
+sudo docker volume rm $(sudo docker volume ls -q)
+sudo docker network rm $(sudo docker network ls -q)
